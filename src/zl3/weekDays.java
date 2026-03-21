fd9e527d08d9cb8d8042b03144f9c6999c8d496d
Filip <filip.ostrowicki@wp.com>
2026-03-21 13:54:03 +0100

    PD-03: fixed mistakes

--- src/zl3/weekDays.java
+++ src/zl3/weekDays.java
@@ -10,47 +10,30 @@ public class weekDays {
         String type = (input <= 5) ? "Dzień roboczy" : " Weekend";
 
         switch (input) {
-            case 1 -> {
-                System.out.println("Poniedziałek: " + type);
-                System.out.println("Godziny odjazdu pierwszego autobusu: " + busHours(input));
-            }
-            case 2 -> {
-                System.out.println("Wtorek: " + type);
-                System.out.println("Godziny odjazdu pierwszego autobusu: " + busHours(input));
-            }
-            case 3 -> {
-                System.out.println("Środa: " + type);
-                System.out.println("Godziny odjazdu pierwszego autobusu: " + busHours(input));
-            }
-            case 4 -> {
-                System.out.println("Czwartek: " + type);
-                System.out.println("Godziny odjazdu pierwszego autobusu: " + busHours(input));
-            }
-            case 5 -> {
-                System.out.println("Piątek: " + type);
-                System.out.println("Godziny odjazdu pierwszego autobusu: " + busHours(input));
-            }
-            case 6 -> {
-                System.out.println("Sobota+ " + type);
-                System.out.println("Godziny odjazdu pierwszego autobusu: " + busHours(input));
-            }
-            case 7 -> {
-                System.out.println("Niedziela: " + type);
-                System.out.println("Godziny odjazdu pierwszego autobusu: " + busHours(input));
-            }
-            default -> {
-                System.out.println("BŁĄD! Podaj liczbę z zakresu 1-7");
-            }
+            case 1 ->
+                    System.out.printf("Poniedziałek: %s \nGodziny odjazdu pierwszego autobusu: %s", type, busHours(1));
+            case 2 -> System.out.printf("Wtorek: %s \nGodziny odjazdu pierwszego autobusu: %s", type, busHours(2));
+            case 3 -> System.out.printf("Środa: %s \nGodziny odjazdu pierwszego autobusu: %s", type, busHours(3));
+            case 4 -> System.out.printf("Czwartek: %s \nGodziny odjazdu pierwszego autobusu: %s", type, busHours(4));
+            case 5 -> System.out.printf("Piątek: %s \nGodziny odjazdu pierwszego autobusu: %s", type, busHours(5));
+            case 6 -> System.out.printf("Sobota: %s \nGodziny odjazdu pierwszego autobusu: %s", type, busHours(6));
+            case 7 -> System.out.printf("Niedziela: %s \nGodziny odjazdu pierwszego autobusu: %s", type, busHours(7));
+            default -> throw new RuntimeException("Podaj liczbę z zakresu 1-7");
+
         }
     }
 
-    static String busHours(int input){
-        if(input <= 5){
-            return "05:30";
-        } else if (input == 6) {
-            return "07:00";
-        } else {
-            return "09:00";
+    static String busHours(int input) {
+        switch (input) {
+            case 1, 2, 3, 4, 5 -> {
+                return "05:30";
+            }
+            case 6 -> {
+                return "07:00";
+            }
+            default -> {
+                return "09:00";
+            }
         }
     }
 }
