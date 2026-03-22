8fec9d576e5fa19c073a29dfbd3c1f9501b0c17c
Filip <filip.ostrowicki@wp.com>
2026-03-22 09:54:11 +0100

    PD-04: added scoreboard and prime numbers programs

---
+++ src/pd04/homework04task2.java
@@ -0,0 +1,18 @@
+package pd04;
+
+public class homework04task2 {
+    public static void main(String[] args) {
+        for (int i = 2; i<=50; i++){
+            int numberOfFactors = 0;
+            for (int j = 1; j <= i; j++) {
+                if(i%j == 0){
+                    numberOfFactors+=1;
+                }
+            }
+            if(numberOfFactors == 2){
+                System.out.println(i);
+            }
+
+        }
+    }
+}
