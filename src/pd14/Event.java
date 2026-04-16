662eb1536e61c1efb1afa9b18ee709fa8a079de5
Filip <filip.ostrowicki@wp.com>
2026-04-16 22:26:49 +0200

    PD-14: created Event processing class

---
+++ src/pd14/Event.java
@@ -0,0 +1,13 @@
+package pd14;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@ToString
+@Getter
+@AllArgsConstructor
+public class Event {
+    private String name;
+    private int id;
+}
